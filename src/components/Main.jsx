76ad5e7b5fce86fb03e0f4021cbf9ae3bc22b32e
Naveen Sharma <ns222280@gmail.com>
2024-08-07 13:24:36 +0530

    send group message api is integerated

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -6,10 +6,11 @@ import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import SendIcon from "@mui/icons-material/Send";
 import Button from "@mui/material/Button";
-import {io} from "socket.io-client";
-import SpeedDialIcon from '@mui/material/SpeedDialIcon';
-import CircularProgress from '@mui/material/CircularProgress';
+import { io } from "socket.io-client";
+import SpeedDialIcon from "@mui/material/SpeedDialIcon";
+import CircularProgress from "@mui/material/CircularProgress";
 import ApiConnector from "../services/ApiConnector";
+import Swal from "sweetalert2";
 
 const Main = () => {
   const apiUrl = process.env.REACT_APP_MAIN_URL;
@@ -18,16 +19,19 @@ const Main = () => {
   const [chatUser, setChatUser] = useState([]);
   const [chatId, setChatId] = useState();
   const [myMessage, setMyMessage] = useState("");
+  const [groupId, setGroupId] = useState();
   const [fileUrl, setFileUrl] = useState("");
   const [receivedChats, setReceivedChats] = useState([]);
-  const [socketData, setSocketData] = useState("")
+  const [socketData, setSocketData] = useState("");
   const chatEndRef = useRef(null);
   const socketRef = useRef();
-  const [loader, setLoader] = useState(false)
+  const [loader, setLoader] = useState(false);
   const [online, setOnline] = useState(true);
+  const [chatGroup, setChatGroup] = useState([]);
+  const [myGroupMessage, setMyGroupMessage] = useState("");
 
-
-useEffect(() => {
+  console.log(groupId);
+  useEffect(() => {
     // socket connection
     socketRef.current = io(`https://chatappbackend-33tk.onrender.com`, {
       credentials: true,
@@ -50,25 +54,33 @@ useEffect(() => {
     };
   }, []);
 
-  useEffect(()=>{
-
-    const handleOnline = ()=>{
-      setOnline(true)
-    }
-    const handleOfline = ()=>{
-      setOnline(false)
-    }
+  useEffect(() => {
+    const handleOnline = () => {
+      setOnline(true);
+      Swal.fire({
+        title: "Good job!",
+        text: "you are online",
+        icon: "success"
+      });
+    };
+    const handleOfline = () => {
+      setOnline(false);
+      Swal.fire({
+        title: "Check Your Internet Connection",
+        text: "you are ofline",
+        icon: "error"
+      });
+    };
 
     window.addEventListener("online", handleOnline);
     window.addEventListener("offline", handleOfline);
-    
-    return ()=> {
+
+    return () => {
       window.addEventListener("online", handleOnline);
       window.addEventListener("offline", handleOfline);
     };
+  }, []);
 
-  },[])
-  
   const logout = () => {
     localStorage.removeItem("token");
     localStorage.removeItem("userData");
@@ -87,144 +99,273 @@ useEffect(() => {
       });
   };
 
-  const scrollToBottom = () => {
-    chatEndRef.current?.scrollIntoView({ behavior: 'smooth' });
+  const getGroupDataForChatById = async (id) => {
+    await ApiConnector.get(`/getGroupDataForChatById/${id}`)
+      .then((res) => {
+        setChatGroup(res?.data?.result);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   };
 
-  const createChat = async ()=>{
+  useEffect(() => {
+    getGroupDataForChatById(groupId);
+  }, [groupId]);
 
-    if(myMessage === "" && fileUrl === ""){
-      return
+  const scrollToBottom = () => {
+    chatEndRef.current?.scrollIntoView({ behavior: "smooth" });
+  };
+
+  const createChat = async () => {
+    if (myMessage === "" && fileUrl === "") {
+      return;
     }
 
     const payload = {
-      userId:userobject?.id,
-      chat:myMessage? myMessage : null,
-      fileUrl:fileUrl? fileUrl : null,
-      receiverId:chatId
-    }
+      userId: userobject?.id,
+      chat: myMessage ? myMessage : null,
+      fileUrl: fileUrl ? fileUrl : null,
+      receiverId: chatId,
+    };
 
     await ApiConnector.post(`/createChat`, payload)
-      .then((res)=>{
+      .then((res) => {
         // socket.emit("message", myMessage)
         socketRef.current.emit("message", myMessage);
-        console.log("message send")
-        setMyMessage("")
-        setFileUrl("")
-        receiveChats()
+        console.log("message send");
+        setMyMessage("");
+        setFileUrl("");
+        receiveChats();
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  useEffect(() => {
+    scrollToBottom();
+  }, [socketData, receivedChats, myMessage]);
+  // ==================================receive chats datas ================================
+
+  const receiveChats = async () => {
+    await ApiConnector.get(
+      `/receiveChats/userId=${userobject?.id}/receiverId=${chatId}`
+    )
+      .then((res) => {
+        setReceivedChats(res?.data?.result);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  useEffect(() => {
+    receiveChats();
+  }, [socketData, chatId]);
+
+  const uploadFile = async (e) => {
+    const payload = {
+      imageFile: e.target.files[0],
+    };
+    setLoader(true);
+    await ApiConnector.post(`/imageUpload`, payload, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    })
+      .then((res) => {
+        console.log(res?.data?.result);
+        setFileUrl(res?.data?.result);
+        setLoader(false);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const sendGroupMessage = async ()=>{
+    const payload = {
+      groupId, 
+      message : myGroupMessage ,
+      userId :userobject?.id,
+    }
+    await ApiConnector.post(`/sendGroupMessage/`, payload)
+      .then((res)=>{
+        console.log("message Send on Group")
+        setMyGroupMessage("")
       })
       .catch((err)=>{
         console.log(err)
       })
   }
 
-  useEffect(()=>{
-    scrollToBottom()
-  },[socketData , receivedChats, myMessage])
-  // ==================================receive chats datas ================================
-
-  const receiveChats = async ()=>{
-    await ApiConnector.get(`/receiveChats/userId=${userobject?.id}/receiverId=${chatId}`,)
-    .then((res)=>{
-      setReceivedChats(res?.data?.result)
-    })
-    .catch((err)=>{
-      console.log(err)
-    })
-  }
-
-useEffect(()=>{
-  receiveChats()
-},[socketData, chatId])
-
-
-  const uploadFile = async (e)=>{
-    const payload = {
-      imageFile:e.target.files[0]
-    }
-    setLoader(true)
-    await ApiConnector.post(`/imageUpload`, payload, {
-      headers: {
-        'Content-Type': 'multipart/form-data',
-      },
-    }) 
-      .then((res)=>{
-        console.log(res?.data?.result)
-        setFileUrl(res?.data?.result)
-        setLoader(false)
-      })
-      .catch((err)=>{
-        console.log(err);
-      })
-
-  }
-
-  console.log(fileUrl)
-
-
   return (
     <main className="bg-gray-700 h-screen">
       <div className="max-w-[1200px] w-[90%] m-auto">
         <div className="flex justify-between h-screen border overflow-hidden rounded-lg">
           <div className="h-full w-[30%]">
-            <Users getUserDataForChatById={getUserDataForChatById} receiveChats={receiveChats} />
+            <Users
+              getUserDataForChatById={getUserDataForChatById}
+              setChatId={setChatId}
+              receiveChats={receiveChats}
+              setGroupId={setGroupId}
+            />
           </div>
 
-          {chatId ? (
+          {chatId || groupId ? (
             <div className="h-full w-[70%] flex flex-col justify-between">
               <div className="click-user bg-gray-500 p-3 text-white flex justify-between items-center gap-2">
-                {chatUser?.map((user, i) => (
-                  <div className="flex items-center gap-2" key={user?.id}>
-                    <Avatar alt="Remy Sharp" src={user?.imageUrl} />
-                    <div>
-                      <p>{user?.userName}</p>
-                      <p>Online</p>
-                    </div>
-                  </div>
-                ))}
+                {chatId &&
+                  chatUser?.map((user, i) => (
+                    <>
 
+                    <div className="flex items-center gap-2" key={user?.id}>
+                      <Avatar alt="Remy Sharp" src={user?.imageUrl} />
+                      <div>
+                        <p>{user?.userName}</p>
+                        <p>Online</p>
+                      </div>
+                    </div>
                 <button onClick={logout}>LogOut</button>
+                    </>
+                  ))}
+                {groupId &&
+                  chatGroup?.map((user, i) => (
+                    <>
+                    <div className="flex items-center gap-2" key={user?.id}>
+                      <Avatar alt="Remy Sharp" src={user?.imageUrl} />
+                      <div>
+                        <p>{user?.name}</p>
+                        <p>Online</p>
+                      </div>
+                    </div>
+                <Button sx={{color:"white"}}>Add Member</Button>
+                    </>
+                  ))}
+
               </div>
+
+{/* ==================================messages=============================== */}
+
               <div className="chats border border-gray-400 flex-grow flex flex-col justify-end items-end relative">
                 <div className="chatbox h-[90%] w-[100%] overflow-auto absolute top-0 text-white p-5">
-                {
-                  receivedChats.map((val, ind)=>(
-                  <span className={`flex flex-col gap-2 ${val?.userId === userobject?.id ? "items-end" : null} `}>
+                  {chatId && receivedChats.map((val, ind) => (
+                    <span
+                      className={`flex flex-col gap-2 ${
+                        val?.userId === userobject?.id ? "items-end" : null
+                      } `}
+                    >
+                      <img
+                        src={val?.imageUrl}
+                        alt=""
+                        className="w-[40%] mt-2"
+                      />
 
-                  <img
-                    src={val?.imageUrl}
-                    alt=""
-                    className="w-[40%] mt-2"
-                  />
+                      <p
+                        className={`bg-gray-600 w-[40%] p-3 rounded-md ${
+                          val?.userId === userobject?.id ? "bg-gray-800" : null
+                        } `}
+                      >
+                        {val?.chat}
 
-                  <p className={`bg-gray-600 w-[40%] p-3 rounded-md ${val?.userId === userobject?.id ? "bg-gray-800" : null} `}>{val?.chat}
+                        <div ref={chatEndRef} />
+                      </p>
+                    </span>
+                  ))}
 
-                  <div ref={chatEndRef} />
-                  </p>
-                  </span>
-                  ))
-                }
+                  {/* {groupId && receivedChats.map((val, ind) => (
+                    <span
+                      className={`flex flex-col gap-2 ${
+                        val?.userId === userobject?.id ? "items-end" : null
+                      } `}
+                    >
+                      <img
+                        src={val?.imageUrl}
+                        alt=""
+                        className="w-[40%] mt-2"
+                      />
+
+                      <p
+                        className={`bg-gray-600 w-[40%] p-3 rounded-md ${
+                          val?.userId === userobject?.id ? "bg-gray-800" : null
+                        } `}
+                      >
+                        {val?.chat}
+
+                        <div ref={chatEndRef} />
+                      </p>
+                    </span>
+                  ))} */}
+
                 </div>
-                <div className="h-[10%] w-full flex items-center bg-gray-500">
-                  <input
-                    type="text"
-                    placeholder="enter your message"
-                    className="w-full  text-white h-full border-none outline-none bg-gray-500 placeholder:text-white p-3"
-                    value={myMessage}
-                    onChange={(e)=>setMyMessage(e.target.value)}
-                  />
-                  <span className="bg-[#1976D2] mx-1 rounded-full cursor-pointer grid place-items-center h-[50px] w-[70px] relative text-white">
-                    <input type="file" name="file" className="border h-full w-full absolute rounded-full opacity-0 cursor-pointer" onChange={(e)=>uploadFile(e)}   />
-                    <SpeedDialIcon className="cursor-pointer" />
-                  </span>
-                  {
-                    loader? <CircularProgress sx={{color:"white"}} /> :
-                  <Button variant="contained" endIcon={<SendIcon />} className="self-stretch" onClick={createChat}>
-                    Send
-                  </Button>
-                  }
 
-                </div>
+
+                {chatId && (
+                  <div className="h-[10%] w-full flex items-center bg-gray-500">
+                    <input
+                      type="text"
+                      placeholder="enter your message"
+                      className="w-full  text-white h-full border-none outline-none bg-gray-500 placeholder:text-white p-3"
+                      value={myMessage}
+                      onChange={(e) => setMyMessage(e.target.value)}
+                    />
+                    <span className="bg-[#1976D2] mx-1 rounded-full cursor-pointer grid place-items-center h-[50px] w-[70px] relative text-white">
+                      <input
+                        type="file"
+                        name="file"
+                        className="border h-full w-full absolute rounded-full opacity-0 cursor-pointer"
+                        onChange={(e) => uploadFile(e)}
+                      />
+                      <SpeedDialIcon className="cursor-pointer" />
+                    </span>
+                    {loader ? (
+                      <CircularProgress sx={{ color: "white" }} />
+                    ) : (
+                      <Button
+                        variant="contained"
+                        endIcon={<SendIcon />}
+                        className="self-stretch"
+                        onClick={createChat}
+                      >
+                        Send
+                      </Button>
+                    )}
+                  </div>
+                )}
+
+                {groupId && (
+                  <div className="h-[10%] w-full flex items-center bg-gray-500">
+                    <input
+                      type="text"
+                      placeholder="enter your group message"
+                      className="w-full  text-white h-full border-none outline-none bg-gray-500 placeholder:text-white p-3"
+                      value={myGroupMessage}
+                      onChange={(e)=>setMyGroupMessage(e.target.value)}
+                    />
+                    <span className="bg-[#1976D2] mx-1 rounded-full cursor-pointer grid place-items-center h-[50px] w-[70px] relative text-white">
+                      <input
+                        type="file"
+                        name="file"
+                        className="border h-full w-full absolute rounded-full opacity-0 cursor-pointer"
+                        // onChange={(e) => uploadFile(e)}
+                      />
+                      <SpeedDialIcon className="cursor-pointer" />
+                    </span>
+                    {loader ? (
+                      <CircularProgress sx={{ color: "white" }} />
+                    ) : (
+                      <Button
+                        variant="contained"
+                        endIcon={<SendIcon />}
+                        className="self-stretch"
+                        onClick={sendGroupMessage}
+                      >
+                        Send
+                      </Button>
+                    )}
+                  </div>
+                )}
               </div>
             </div>
           ) : (
@@ -233,11 +374,9 @@ useEffect(()=>{
                 className="flex justify-center bg-gray-500 rounded-lg text-5xl text-white w-full h-full items-center text-center"
                 style={{ textShadow: "0 0 20px" }}
               >
-              {
-                online === true? 
-                "Welcome to our chat Applicaton ": "Please check your internet Connection you are ofline"
-
-              }
+                {online === true
+                  ? "Welcome to our chat Applicaton "
+                  : "Please check your internet Connection you are ofline"}
               </p>
             </div>
           )}
--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -3,75 +3,223 @@ import Avatar from "@mui/material/Avatar";
 import SearchIcon from "@mui/icons-material/Search";
 import IconButton from "@mui/material/IconButton";
 import InputBase from "@mui/material/InputBase";
-import axios from "axios"
-import {GlobalUserData} from "../App"
+import { GlobalUserData } from "../App";
 import ApiConnector from "../services/ApiConnector";
+import Box from "@mui/material/Box";
+import Tab from "@mui/material/Tab";
+import TabContext from "@mui/lab/TabContext";
+import TabList from "@mui/lab/TabList";
+import TabPanel from "@mui/lab/TabPanel";
+import PersonIcon from "@mui/icons-material/Person";
+import GroupsIcon from "@mui/icons-material/Groups";
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Swal from "sweetalert2";
 
-const Users = ({getUserDataForChatById, receiveChats}) => {
-
+const Users = ({ getUserDataForChatById, receiveChats , setGroupId, setChatId }) => {
   const apiUrl = process.env.REACT_APP_MAIN_URL;
-  const [searchQuery, setSearchQuery] = useState("all")
-  const [chatUsers, setChatUsers] = useState([])
+  const [searchQuery, setSearchQuery] = useState("all");
+  const [chatUsers, setChatUsers] = useState([]);
+  const [value, setValue] = React.useState("1");
+  const [open, setOpen] = React.useState(false);
+  const [groupName, setGroupName] = React.useState('');
+  const { userobject } = useContext(GlobalUserData);
+  const [groups, setGroups] = React.useState([]);
+  
 
-  const {userobject} = useContext(GlobalUserData);
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
 
-  const getUserDataForChat = async ()=>{
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const getUserDataForChat = async () => {
     await ApiConnector.get(`/getUserDataForChat/${searchQuery}`)
-      .then((res)=>{
+      .then((res) => {
         setChatUsers(res?.data?.result);
       })
-      .catch((err)=>{
-        console.log(err.message)
+      .catch((err) => {
+        console.log(err.message);
+      });
+  };
+
+  const createGroup = async () => {
+    await ApiConnector.post(`/createGroup/${groupName}`)
+      .then((res) => {
+        Swal.fire({
+          title: "Good job!",
+          text: "Group Created Successfully",
+          icon: "success"
+        });
+        setGroupName("")
+        receiveGroup()
+        handleClose()
       })
-  }
+      .catch((err) => {
+        console.log(err.message);
+      });
+  };
+
+  const receiveGroup = async () => {
+    await ApiConnector.get(`/receiveGroup`)
+      .then((res) => {
+        setGroups(res?.data?.result)
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  };
 
   useEffect(()=>{
-    getUserDataForChat()
-  },[searchQuery])
+    receiveGroup()
+  },[])
 
-  useEffect(()=>{
+  useEffect(() => {
+    getUserDataForChat();
+  }, [searchQuery]);
+
+  useEffect(() => {
     function setQery(params) {
-      setSearchQuery("all")
+      setSearchQuery("all");
     }
-    setQery()
-  },[searchQuery.length === 0])
-
+    setQery();
+  }, [searchQuery.length === 0]);
 
   return (
-    <>  
-        <div className="bg-gray-500 flex justify-center items-center m-3 rounded-md">
-        <InputBase
-          sx={{ ml: 1, flex: 1, color:"white", width:"100%"}}
-          placeholder="Search User"
-          inputProps={{ "aria-label": "search google maps" }}
-          onChange={(e)=>setSearchQuery(e.target.value)}
-        />
-        <IconButton type="button" sx={{ p: "10px" }} aria-label="search">
-          <SearchIcon sx={{color:"white"}} />
-        </IconButton>
-        </div>
+    <>
+      <Box sx={{ width: "100%", typography: "body1" }}>
+        <TabContext value={value}>
+          <div className="border flex justify-between rounded-sm">
+            <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+              <TabList
+                onChange={handleChange}
+                aria-label="lab API tabs example"
+              >
+                <Tab label={<PersonIcon sx={{ color: "white" }} />} value="1" />
+                <Tab label={<GroupsIcon />} sx={{ color: "white" }} value="2" />
+              </TabList>
+            </Box>
+            <button className="text-white px-3" onClick={handleClickOpen}>Create Group</button>
+          </div>
+          <TabPanel value="1">
+            <div>
+              <div className="bg-gray-500 flex justify-center items-center rounded-md">
+                <InputBase
+                  sx={{ ml: 1, flex: 1, color: "white", width: "100%" }}
+                  placeholder="Search User"
+                  inputProps={{ "aria-label": "search google maps" }}
+                  onChange={(e) => setSearchQuery(e.target.value)}
+                />
+                <IconButton
+                  type="button"
+                  sx={{ p: "10px" }}
+                  aria-label="search"
+                >
+                  <SearchIcon sx={{ color: "white" }} />
+                </IconButton>
+              </div>
 
-    {/* ========================users========================= */}
+              {/* ========================users========================= */}
 
-      { chatUsers.map((user, i)=>(
-        userobject?.id === user?.id? null :
-      <div key={user?.id} className="flex justify-between items-center bg-gray-500 p-2 m-3 rounded-md cursor-pointer" onClick={()=>{getUserDataForChatById(user?.id) ; receiveChats()} }>
+              {chatUsers.map((user, i) =>
+                userobject?.id === user?.id ? null : (
+                  <div
+                    key={user?.id}
+                    className="flex justify-between items-center bg-gray-500 p-2 my-3 rounded-md cursor-pointer"
+                    onClick={() => {
+                      getUserDataForChatById(user?.id);
+                      receiveChats();
+                      setGroupId(false)
+                    }}
+                  >
+                    <div className="w-[15%]">
+                      <Avatar alt="Remy Sharp" src={user?.imageUrl} />
+                    </div>
 
-        <div className="w-[15%]">
-          <Avatar
-            alt="Remy Sharp"
-            src={user?.imageUrl}
-          />
-        </div>
+                    <div className="w-[85%] text-white">
+                      <p>{user?.userName}</p>
+                      <p className="text-gray-300">recent message</p>
+                    </div>
+                  </div>
+                )
+              )}
+            </div>
+          </TabPanel>
+          <TabPanel value="2">
+            <div>
+              <div className="bg-gray-500 flex justify-center items-center rounded-md">
+                <InputBase
+                  sx={{ ml: 1, flex: 1, color: "white", width: "100%" }}
+                  placeholder="Search Group"
+                  inputProps={{ "aria-label": "search google maps" }}
+                />
+                <IconButton
+                  type="button"
+                  sx={{ p: "10px" }}
+                  aria-label="search"
+                >
+                  <SearchIcon sx={{ color: "white" }} />
+                </IconButton>
+              </div>
 
-        <div className="w-[85%] text-white">
-          <p>{user?.userName}</p>
-          <p className="text-gray-300">recent message</p>
-        </div>
-      </div>
-    ))
+              {/* ========================users========================= */}
 
-      }
+              {
+                groups.map((group, i)=>(
+              <div
+                className="flex justify-between items-center bg-gray-500 p-2 my-3 rounded-md cursor-pointer"
+                onClick={() => {
+                  setGroupId(group?.id);
+                  setChatId(false)
+                    }}
+              >
+                  <div className="w-[15%]" key={group?.id}>
+                  <Avatar alt="Remy Sharp" src={group?.imageUrl} />
+                </div>
+
+                <div className="w-[85%] text-white">
+                  <p>{group?.name}</p>
+                  <p className="text-gray-300">recent message</p>
+                </div>
+              </div>
+                ))
+              }
+            </div>
+          </TabPanel>
+        </TabContext>
+      </Box>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">
+          {"Create Group"}
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+          <TextField id="standard-basic" label="GroupName" variant="standard" value={groupName} onChange={(e)=>setGroupName(e.target.value)} />
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Cancel</Button>
+          <Button onClick={()=>createGroup()} autoFocus>
+            Create
+          </Button>
+        </DialogActions>
+      </Dialog>
     </>
   );
 };
